cc4565aa8d1d922ae8266011a9c2a48a069c8796
marvin <mcanastuj1999@gmail.com>
2026-03-03 21:24:40 -0600

    adding functionality for modal window and creating a function to show the cards

--- src/main.js
+++ src/main.js
@@ -3,3 +3,46 @@
  * Este bloque de código contiene la funcionalidad principal
  * que define el comportamiento del programa.
  */
+
+// Import functions from  utils.js 
+import { activeToggleModal, modalSearch, createCard} from "./utils"
+
+// Import array from stays.js
+import {stays} from "./stays"
+
+
+// open and close toggle 
+let searchMain = document.querySelector("#search_main")
+
+searchMain.addEventListener("click", () => {
+    activeToggleModal()
+})
+
+let closeButtonModal = document.querySelector("#close_button__modal")
+
+closeButtonModal.addEventListener("click", () => {
+    activeToggleModal()
+})
+
+modalSearch.addEventListener("click", () => {
+    activeToggleModal()
+})
+
+let modalSearchForm = document.querySelector("#modal_search__form")
+
+modalSearchForm.addEventListener("click", (e) => {
+    e.stopPropagation()
+})
+
+let buttonSearchModal = document.querySelector("#button_search_modal")
+
+buttonSearchModal.addEventListener("click", () => {
+    activeToggleModal()
+})
+
+
+//metodo input
+
+stays.forEach(element => {
+    createCard(element)
+});
--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,52 @@
  * Este archivo contiene funciones auxiliares que serán utilizadas y llamadas
  * desde el archivo principal para realizar varias operaciones.
  */
+
+// To open and close modal window
+export let modalSearch = document.querySelector("#modal_search");
+let body = document.querySelector("body");
+
+export function activeToggleModal() {
+    modalSearch.classList.toggle("hidden");
+    body.classList.toggle("overflow-hidden");
+}
+
+// To create a card
+
+let cardsSection = document.querySelector("#cards_section");
+
+export function createCard(element) {
+    let card = ``
+    card = `
+    <article class="hover:shadow-lg rounded-2xl p-2">
+            <div class="overflow-hidden rounded-2xl aspect-8/5">
+                <img src=${element.photo} alt="${element.type}" class="rounded-2xl object-cover w-full h-full object-center transition-all duration-500 hover:scale-120 ">
+            </div>
+            
+            <div class="flex justify-between mt-3 items-center">
+    `
+    if (element.superHost === true) {
+        card += `
+        <p class="border rounded-xl font-bold p-1 text-xs text-[#3b3b3b] ">SUPERHOST</p>
+        `
+    }
+    if (element.beds === null) {
+        card += `<p class="text-[#818181] font-semibold text-sm mx-1 text-center">${element.type}</p>`
+    } else {
+        card += `<p class="text-[#818181] font-semibold text-sm mx-1 text-center">${element.type}. ${element.beds} beds</p>`
+    }
+    card += 
+    `
+        <div class="flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5 text-[#eb5757] fill-[#eb5757] ">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+                    </svg>
+                    <p class="text-[#3b3b3b] font-semibold text-sm">${element.rating}</p>
+                </div>
+            </div>
+            <p class="font-bold text-[#333333] text-sm my-1">${element.title}</p>
+        </article>
+    `
+    cardsSection.innerHTML += card
+}
+
